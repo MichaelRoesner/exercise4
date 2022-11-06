cc6c6b6b8ae8158829c7ff79138dcdc7994be65e
g1457 <93216360+g1457@users.noreply.github.com>
2022-11-06 16:59:07 +0100

    Add files via upload

---
+++ src/de/hsh/dbs2/imdb/entities/Genre.java
@@ -0,0 +1,92 @@
+package de.hsh.dbs2.imdb.entities;
+
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import de.hsh.dbs2.imdb.util.DBConnection;
+
+public class Genre {
+    public static final String seq_genreID = "genre_id";
+    public static final String table = "Genre";
+    public static final String col_genre ="Genre";
+    public static final String col_genreID = "GenreID";
+
+    private String genre; // Spalte: Genre
+    private int genreID; // Spalte: GenreID
+
+
+    /**
+     * Initialisiert die Entität.
+     * @param genre Spalte: Genre
+     * @param genreID Spalte: GenreID
+     */
+    public Genre(String genre, int genreID) {
+        this.setGenre(genre);
+        this.setGenreID(genreID);
+    }
+
+    public Genre(){}
+
+    public void insert() throws SQLException {
+        // SQL-Statement:
+        String sql ="INSERT INTO " + table+" VALUES (" + seq_genreID + ".nextval, ?)";
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement(sql);
+        stmt.setString(1, this.getGenre());
+        int rowsInserted = stmt.executeUpdate();
+        System.out.println("Es wurden "+rowsInserted+" Zeilen hinzugefügt");
+
+        //ID:
+        sql ="Select "+seq_genreID+".currval From DUAL";
+        stmt= DBConnection.getConnection().prepareStatement(sql);
+        ResultSet rs=stmt.executeQuery();
+        if(rs.next())this.setGenreID(rs.getInt(1));
+        rs.close();
+        stmt.close();
+    }
+
+
+    public void update() throws SQLException {
+        // SQL-Statement:
+        String sql ="UPDATE " + table+ " SET " + col_genre + " = ?"+" WHERE " + col_genreID + " = ?";
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement(sql);
+        stmt.setString(1, this.getGenre());
+        stmt.setInt(2, this.getGenreID());
+        int rowsUpdated = stmt.executeUpdate();
+        System.out.println("Es wurden "+rowsUpdated+" Zeilen verändert");
+        stmt.close();
+    }
+
+
+    public void delete() throws SQLException {
+        // SQL-Statement
+        String sql ="DELETE FROM " + table+" WHERE " + col_genreID + " = ?";
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement(sql);
+        stmt.setInt(1, this.getGenreID());
+        int rowsDeleted = stmt.executeUpdate();
+        System.out.println("Es wurden "+rowsDeleted+" Zeilen gelöscht");
+        stmt.close();
+    }
+
+
+    public String getGenre() {
+        return genre;
+    }
+
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+
+    public int getGenreID() {
+        return genreID;
+    }
+
+
+    public void setGenreID(int genreID) {
+        this.genreID = genreID;
+    }
+}
---
+++ src/de/hsh/dbs2/imdb/entities/MovieGenre.java
@@ -0,0 +1,91 @@
+package de.hsh.dbs2.imdb.entities;
+
+
+import de.hsh.dbs2.imdb.util.DBConnection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class MovieGenre {
+    public static final String table = "hasGenre";
+    public static final String col_genreID = "GenreID";
+    public static final String col_movieID = "MovieID";
+
+    private int genreID; // Spalte: GenreID
+    private int movieID; // Spalte: MovieID
+
+
+    /**
+     * Initialisiert die Entität.
+     * @param genreID Spalte: GenreID
+     * @param movieID Spalte: MovieID
+     */
+    public MovieGenre(int genreID, int movieID) {
+        this.setGenreID(genreID);
+        this.setMovieID(movieID);
+    }
+
+    public MovieGenre(){}
+
+    public void insert() throws SQLException {
+        // SQL-Statement:
+        String sql = "INSERT INTO " + table +" VALUES (?, ?)";
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement(sql);
+        stmt.setInt(1, this.getMovieID());
+        stmt.setInt(2, this.getGenreID());
+
+        // Insert:
+        int rowsInserted = stmt.executeUpdate();
+        System.out.println("Es wurden "+rowsInserted+" Zeilen hinzugefügt");
+        stmt.close();
+    }
+
+
+    public void update() throws SQLException {
+        // SQL-Statement:
+        String  sql ="UPDATE " + table+" SET " + col_movieID + " = ?, " + col_genreID + " = ? WHERE " + col_movieID + " = ? AND " + col_genreID + " = ?";
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement(sql);
+        stmt.setInt(1, this.getMovieID());
+        stmt.setInt(2, this.getGenreID());
+        stmt.setInt(3, this.getMovieID());
+        stmt.setInt(4, this.getGenreID());
+
+        // Update:
+        int rowsUpdated = stmt.executeUpdate();
+        System.out.println("Es wurden "+rowsUpdated+" Zeilen verändert");
+        stmt.close();
+    }
+
+    public void delete() throws SQLException {
+        // SQL-Statement
+        String sql = "DELETE FROM " + table +"WHERE " + col_movieID + " = ? AND " + col_genreID + " = ?";
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement(sql.toString());
+        stmt.setInt(1, this.getMovieID());
+        stmt.setInt(2, this.getGenreID());
+
+        // Delete:
+        int rowsDeleted = stmt.executeUpdate();
+        System.out.println("Es wurden "+rowsDeleted+" Zeilen gelöscht");
+        stmt.close();
+    }
+
+
+    public int getGenreID() {
+        return genreID;
+    }
+
+
+    public void setGenreID(int genreID) {
+        this.genreID = genreID;
+    }
+
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+}
